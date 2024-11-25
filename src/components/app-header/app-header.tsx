c354c2479bae8949b43084a88892e5c6ba67be7c
JetMod <enver027@yandex.ru>
2024-11-25 21:24:13 +0300

    updated

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,16 +1,15 @@
 import { FC, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { AppHeaderUI } from '@ui';
-import { selectUserData, verifyUserAuth } from '../../slices/authSlice'; // Импортируйте ваш селектор и thunk
+import { selectUserData, verifyUserAuth } from '../../slices/authSlice';
 
 export const AppHeader: FC = () => {
   const dispatch = useDispatch();
-  const user = useSelector(selectUserData); // Получаем данные пользователя
-  const userName = user ? user.name : ''; // Получаем имя пользователя
+  const user = useSelector(selectUserData);
+  const userName = user ? user.name : '';
 
   useEffect(() => {
-    // Проверяем авторизацию пользователя при загрузке
-    dispatch(verifyUserAuth() as any); // Приведение типа для обхода ошибки
+    dispatch(verifyUserAuth() as any);
   }, [dispatch]);
 
   return <AppHeaderUI userName={userName} />;
